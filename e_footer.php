425c126138b0dae98ed55531efcab53511595fef
Arun S. Sekher <arunshekher@users.noreply.github.com>
2017-08-04 05:27:54 +0400

    Correct global pref implementation (untested), Remove unused node from plugin.xml

--- e_footer.php
+++ e_footer.php
@@ -2,7 +2,7 @@
 
 $livetimePref = e107::getPlugPref('livetime');
 
-if ($livetimePref) {
+if ($livetimePref['livetime_active']) {
 	e107::js('footer', '{e_PLUGIN}livetime/js/libs/moment/moment.js', 'jquery', 1);
 	e107::js('footer', '{e_PLUGIN}livetime/js/libs/livestamp/livestamp.js', 'jquery', 1);
 }
