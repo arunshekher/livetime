6d97044f800cd72cd5f01289283776a0345ac1f8
Arun S. Sekher <arunshekher@users.noreply.github.com>
2017-08-06 06:25:06 +0400

    Implement livetime_active and global_libs preferences.

--- e_footer.php
+++ e_footer.php
@@ -3,6 +3,31 @@
 $livetimePref = e107::getPlugPref('livetime');
 
 if ($livetimePref['livetime_active']) {
-	e107::js('footer', '{e_PLUGIN}livetime/js/libs/moment/moment.js', 'jquery', 1);
-	e107::js('footer', '{e_PLUGIN}livetime/js/libs/livestamp/livestamp.js', 'jquery', 1);
+
+	if ($livetimePref['global_libs']) {
+		loadLibsGlobally();
+	} else {
+		loadLibsLocally();
+	}
+}
+
+
+
+/**
+ * Loads libs from the local path
+ */
+function loadLibsLocally()
+{
+	e107::js('footer', '{e_PLUGIN}livetime/js/libs/moment/min/moment.min.js', 'jquery', 1);
+	e107::js('footer', '{e_PLUGIN}livetime/js/libs/livestamp/livestamp.min.js', 'jquery', 1);
+}
+
+
+/**
+ * Loads libs from the global path
+ */
+function loadLibsGlobally()
+{
+	e107::library('load', 'moment.js', 'minified');
+	e107::library('load', 'livestamp.js', 'minified');
 }
